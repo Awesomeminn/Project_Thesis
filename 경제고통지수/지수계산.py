bef0dd24bc9841586f469d5f53e22696094ccb01
Minjae Kim <pietromin98j@naver.com>
2025-05-05 15:49:57 +0900

    쓸데없는 거 다 지움

--- 경제고통지수/지수계산.py
+++
@@ -1,8 +0,0 @@
-# 모든 코드 맨 위에 이거 복붙하셈
-import os 
-os.getcwd()
-os.chdir('C:/Users/Awesomemin/Desktop/연구아카이브/Project_Thesis/경제고통지수')
-os.getcwd()
-#=======================================================================================================#
-# 실업률 raw data (한국은행 Open API)
-
